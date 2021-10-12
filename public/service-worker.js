cc85b8bf0339b9db71b58d979f332152dba7e565
mjos7 <83667780+mjos7@users.noreply.github.com>
2021-10-11 20:48:13 -0700

    add data cache name

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,7 @@
 const APP_PREFIX = 'EasyBudgetTracker-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
+const DATA_CACHE_NAME = 'data-cache-v1';
 const FILES_TO_CACHE = [
   '/',
   './index.html',
@@ -30,44 +31,52 @@ self.addEventListener('install', function (e) {
       return cache.addAll(FILES_TO_CACHE);
     })
   );
+  self.skipWaiting();
 });
 
 // Delete outdated caches
-self.addEventListener('activate', function (e) {
-  e.waitUntil(
-    caches.keys().then(function (keyList) {
-      // `keyList` contains all cache names under your username.github.io
-      // filter out ones that has this app prefix to create keeplist
-      let cacheKeeplist = keyList.filter(function (key) {
-        return key.indexOf(APP_PREFIX);
-      });
-      // add current cache name to keeplist
-      cacheKeeplist.push(CACHE_NAME);
-
+self.addEventListener('activate', function (evt) {
+  evt.waitUntil(
+    caches.keys().then(keyList => {
       return Promise.all(
-        keyList.map(function (key, i) {
-          if (cacheKeeplist.indexOf(key) === -1) {
-            return caches.delete(keyList[i]);
+        keyList.map(key => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log('Removing old cache data', key);
+            return caches.delete(key);
           }
         })
       );
     })
   );
+  self.clients.claim();
 });
 
 // Respond with cached resources
-self.addEventListener('fetch', function (e) {
-  e.respondWith(
-    caches.match(e.request).then(function (request) {
-      if (request) {
-        // if cache is available, respond with cache
-        console.log('responding with cache : ' + e.request.url);
-        return request;
-      } else {
-        // if there are no cache, try fetching request
-        console.log('file is not cached, fetching : ' + e.request.url);
-        return fetch(e.request);
-      }
+self.addEventListener('fetch', function (evt) {
+  if (evt.request.url.includes('/api/transaction')) {
+    console.log('[Service Worker] Fetch (data)', evt.request.url);
+
+    evt.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(evt.request)
+          .then(response => {
+            if (response.status === 200) {
+              cache.put(evt.request.url, response.clone());
+            }
+            return response;
+          })
+          .catch(err => {
+            return cache.match(evt.request);
+          });
+      })
+    );
+    return;
+  }
+  evt.respondWith(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.match(evt.request).then(response => {
+        return response || fetch(evt.request);
+      });
     })
   );
 });
