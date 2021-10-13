b28eb9766ddf7c812d19ac4f3cb255c88b15f0a6
mjos7 <83667780+mjos7@users.noreply.github.com>
2021-10-13 15:43:21 -0700

    remove bootstrap and charts from file cache to try and debug

--- public/service-worker.js
+++ public/service-worker.js
@@ -18,9 +18,9 @@ const FILES_TO_CACHE = [
   './icons/icon-128x128.png',
   './icons/icon-96x96.png',
   './icons/icon-72x72.png',
-  'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/fonts/fontawesome-webfont.woff?v=4.7.0',
-  'https://cdn.jsdelivr.net/npm/chart.js@2.8.0',
-  'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
+  // 'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/fonts/fontawesome-webfont.woff?v=4.7.0',
+  // 'https://cdn.jsdelivr.net/npm/chart.js@2.8.0',
+  // 'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
 ];
 
 self.addEventListener(`install`, event => {
